ed2d422646a79a121705a806018789f226a6d979
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-10-16 11:26:55 -0700

    a

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckOutController;
+use App\Http\Controllers\ContactUsController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\QuotationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,23 +21,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-
 //Home
 Route::get('/', [HomeController::class, "index"])->name('home');
 Route::get('/about-us', [HomeController::class, "about"])->name('about');
+
+Route::prefix('products')->group(function (){
+    Route::get('/',[ProductController::class, 'index'])->name('products.all');
+});
 
 Route::prefix('contact-us')->group(function () {
     Route::get('/', [ContactUsController::class,'contact'])->name('contact');
@@ -38,5 +35,21 @@ Route::prefix('contact-us')->group(function () {
 
 Route::prefix('checkout')->group(function () {
     Route::get('/', [CheckOutController::class,'index'])->name('checkout');
+});
+
+Route::prefix('orders')->group(function () {
+    Route::get('/', [OrderController::class,'index'])->name('orders');
+});
+
+Route::prefix('cart')->group(function () {
+    Route::get('/', [CartController::class,'index'])->name('cart');
+});
+
+Route::prefix('MyAccount')->group(function () {
+    Route::get('/', [CustomerController::class,'index'])->name('MyAccount');
+});
+
+Route::prefix('quotation')->group(function () {
+    Route::get('/', [QuotationController::class,'index'])->name('quotation');
 });
 
