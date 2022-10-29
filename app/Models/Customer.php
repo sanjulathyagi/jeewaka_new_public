789dbe9b9d71513d24f5a636b0f70c37938b3c3c
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-10-28 22:52:42 -0700

    a

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
     use HasFactory;
+    use TwoFactorAuthenticatable;
+
     protected $fillable = [
         'name',
         'email',
