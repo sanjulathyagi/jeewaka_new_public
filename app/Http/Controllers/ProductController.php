a0b722612bd9aa3e24cb28918e5979b82938f140
sanjula thyagi <62659908+sanjulathyagi@users.noreply.github.com>
2022-11-11 10:50:39 -0800

    a

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use domain\Facades\CategoryFacade;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
+        $response['categories'] = CategoryFacade::all();
         return view('Pages.Product.index');
     }
 }
--- resources/views/Pages/Product/index.blade.php
+++ resources/views/Pages/Product/index.blade.php
@@ -6,7 +6,15 @@
     <div class="widget catagory mb-50">
         <!-- Widget Title -->
         <h6 class="widget-title mb-30">Search</h6>
+        <!--  Catagories  -->
+        <div class="catagories-menu">
+            <input type="text" class="form-control">
+        </div>
+    </div>
 
+    <div class="widget catagory mb-50">
+        <!-- Widget Title -->
+        <h6 class="widget-title mb-30">Categories</h6>
         <!--  Catagories  -->
         <div class="catagories-menu">
             <input type="text" class="form-control">
@@ -25,13 +33,23 @@
     <div class="widget catagory mb-50">
         <!-- Widget Title -->
         <h6 class="widget-title mb-30">Min Price</h6>
-
         <!--  Catagories  -->
         <div class="catagories-menu">
-            <input type="number" class="form-control">
+            <input type="number" class="form-control" min="0" id="search_min_price" name="min_price">
 
         </div>
     </div>
+
+    <div class="widget catagory mb-50">
+        <!-- Widget Title -->
+        <h6 class="widget-title mb-30">Max Price</h6>
+        <!--  Catagories  -->
+        <div class="catagories-menu">
+            <input type="number" class="form-control" min="0" id="search_max_price" name="max_price">
+
+        </div>
+    </div>
+
 
 
 </div>
